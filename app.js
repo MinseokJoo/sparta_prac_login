d3db8b643ee4f02ef8bd24df17e443cf87ca5a18
Joominseok <jumseok12@gmail.com>
2022-12-29 20:31:46 +0900

    라우터 생성

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ const express = require("express")
 
 const app = express()
 
+app.get("/", (req, res) => {
+  res.send("Hello World~~!")
+})
+
+app.get("/users", (req,res) => {
+  res.send("회원정보 페이지")
+})
+
+app.post("/login", (req,res) => {
+  res.send("로그인 페이지")
+})
+
+app.post("/logout", (req,res) => {
+  res.send("로그아웃 페이지")
+})
+
+app.post("/register", (req,res) => {
+  res.send("register 페이지")
+})
+
+
 app.listen(3000, () => {
   console.log(3000,"번 서버 열림")
 })
