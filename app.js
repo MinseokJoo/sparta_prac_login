cb407f8af996f140d2989ddae1e89c4432d1b985
Joominseok <jumseok12@gmail.com>
2022-12-30 02:50:21 +0900

    마지막으로 하나도 안보고 적었다. try catch를 써보고 싶었다...하하하하하 분명 틀린 게 있을 것 같다...

--- app.js
+++ app.js
@@ -13,28 +13,31 @@ const users = [
   {name: "한동주", id: "dongjoo", password: "vded"},
 ]
 
-app.get("/login", (req, res) => {
+app.get("/login", (req,res) =>{
   const id = req.query.id
   const password = req.query.password
 
-  const user = users.find(user => user.id === id)
-  
-  if (!user) {
-    return res.status(500).json({message: "존재하지 않는 아이디 입니다."})
+  try {
+    const user = users.find(user => user.id === id)
+    
+    if (!user) {
+      return res.status(401).json({message: "아이디를 잘못 입력했습니다. 입력하신 내용을 다시 확인해주세요."})
+    }
+
+    if (password !== user.password) {
+      return res.status(401).json({message: "비밀번호를 잘못 입력했습니다. 입력하신 내용을 다시 한번 확인해주세요."})
+    }
+    res.cookie("user_id", user.id)
+    console.log(req.cookies.user_id)
+    res.send("로그인")
+  } catch (err) {
+    res.status(500).json({message: err.message})
   }
-
-  if (user.password !== password) {
-    return res.status(500).json({message: "잘못된 비밀번호 입니다."})
-  }
-
-  res.cookie("user_id", user.id)
-  console.log(req.cookies.user_id)
-
-  res.send("로그인")
 })
 
-app.get("/logout", (_,res) => {
+app.get("/logout", (req,res) => {
   res.clearCookie("user_id")
+  console.log(req.cookies.user_id)
   res.send("로그아웃")
 })
 
@@ -43,31 +46,31 @@ app.get("/register", (req,res) => {
   const password = req.query.password
   const name = req.query.name
 
-  const user = users.find(user => user.id === id)
-
-  if (user) {
-    return res.status(500).json({message: "이미 존재하는 아이디 입니다."})
+  try {
+    const user = users.find(user => user.id === id)
+    if (user) {
+      return res.status(401).json({message: "이미 존재하는 아이디 입니다."})
+    }
+    users.push({name, id, password})
+    console.log(users)
+    res.status(201).send("회원가입")
+  } catch (err) {
+    res.status(500).json({message: err.message})
   }
-
-  users.push({name, id, password})
-  console.log(users)
-
-  res.send("회원가입")
 })
 
-app.get("/users", (req,res) => {
+app.get("/userInfos", (req,res) => {
   const yesLogin = req.cookies.user_id
 
-  if (!yesLogin) {
-    return res.status(500).json({message: "로그인 후 가능한 기능입니다."})
+  try {
+    if (!yesLogin) {
+      return res.json({message: "로그인 이후 사용 가능한 기능입니다."})
+    }
+    const userInfo = users.find(user => user.id === yesLogin)
+    res.send(userInfo)
+  } catch (err) {
+    res.status(500).json({message: err.message})
   }
-  
-  const userInfo = users.find(user => user.id === yesLogin)
-  
-  // userInfo의 값이 없다면 회원정보를 잘못 넣었다는 return을 만들지 않은 이유는 쿠키를 만들려면 로그인을 해야한다는 조건하에 저 쿠키가 없다면
-  // 로그인을 하지않았다! 로그인을 했더라면 userInfo에 값이 존재해야 한다고 판단을 해서 따로 if로 만들지 않았다!
-
-  res.send(userInfo)
 })
 
 app.listen(3000, () => console.log(3000,"번 포트로 서버 열림"))
