ba88394e918f217d8200de7cf86472bff8737d40
Joominseok <jumseok12@gmail.com>
2022-12-30 00:40:03 +0900

    조금 상세한 수도코드를 보며 만든 쿠키코드 유저 정보페이지에서 쿠키가 없다면 로그인 후 가능하다는 메시지로 조금 축약했음.

--- app.js
+++ app.js
@@ -4,26 +4,72 @@ const cookieParser = require("cookie-parser")
 const app = express()
 app.use(cookieParser())
 
+const users = [
+  {name: "우준호", id: "noggong", password: "1234"},
+  {name: "이설인", id: "seolin", password: "asdf"},
+  {name: "주민석", id: "minseok", password: "hello"},
+  {name: "유희선", id: "heesun", password: "94kf"},
+  {name: "한동주", id: "dongjoo", password: "vded"},
+]
+
 app.get("/", (req, res) => {
   res.send("Hello World~~!")
 })
 
 app.get("/users", (req,res) => {
-  console.log(req.cookies.test1)
-  console.log(req.cookies.test2)
-  res.send("회원정보 페이지")
+  const YseLogin = req.cookies.user_id
+
+  if (!YseLogin) {
+    return res.status(500).json({message: "로그인 후 가능한 기능입니다."})
+  }
+  
+  const user = users.find(user => user.id === YseLogin)
+
+  res.send(user)
 })
 
-app.post("/login", (req,res) => {
-  res.send("로그인 페이지")
+app.get("/login", (req,res) => {
+  const id = req.query.id
+  const password = req.query.password
+
+  const user = users.find(user => user.id === id)
+  console.log(user)
+
+  if (!user) {
+    return res.status(500).json({message: "존재하지 않는 아이디입니다."})
+  }
+  
+  if (user.password !== password) {
+    return res.status(500).json({message: "잘못된 비밀번호 입니다."})
+  }
+
+  res.cookie("user_id", user.id)
+  console.log(req.cookies.user_id)
+
+  res.send("로그인")
 })
 
-app.post("/logout", (req,res) => {
-  res.send("로그아웃 페이지")
+app.get("/logout", (req,res) => {
+  res.clearCookie("user_id")
+  res.send("로그아웃")
 })
 
-app.post("/register", (req,res) => {
-  res.send("register 페이지")
+app.get("/register", (req,res) => {
+  const id = req.query.id
+  const password = req.query.password
+  const name = req.query.name
+
+  const user = users.find(user => user.id === id)
+
+  if (user) {
+    return res.status(500).json({message: "이미 존재한 아이디 입니다."})
+  }
+
+  users.push({name, id, password})
+
+  console.log(users)
+
+  res.send("회원가입")
 })
 
 
